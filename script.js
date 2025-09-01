e3f1f683e2e9f24e880c69be632488353053793c
kissabon0803-design <kissabon0803@gmail.com>
2025-09-02 08:47:38 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 let count =0;
-document.getElementById("button1").addEventListener("click",function() {
-count ++;
-document.getElementById("result").textContent = count;
-});
-document.getElementById("button2").addEventListener("click",function(){
-count --;
-document.getElementById("result").textContent = count;
-});
-document.getElementById("reset").addEventListener("click",function(){
-count = 0;
-document.getElementById("result").textContent =count;    
+document.getElementById("button1").addEventListener("click",function(){
+count++ ;
+document.getElementById("result").textContent = "カウント" + count;
 });
